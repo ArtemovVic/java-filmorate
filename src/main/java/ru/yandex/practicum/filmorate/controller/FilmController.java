0c86b3b8611f2c7988ed710c5a3b01a52aeab9ef
Artemov Victor <ArtemovVic23@yandex.ru>
2024-11-13 13:12:37 +0300

    add GenreController/MPaController

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -5,83 +5,56 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.dto.FilmDto;
-import ru.yandex.practicum.filmorate.dto.GenreDto;
-import ru.yandex.practicum.filmorate.dto.RatingMPADto;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.service.FilmDBService;
 
 import java.util.Collection;
-import java.util.List;
 
 @RestController
-//@RequestMapping("/films")
+@RequestMapping("/films")
 @Validated
 public class FilmController {
 
     private final FilmDBService filmService;
 
-
     @Autowired
     public FilmController(FilmDBService filmService) {
         this.filmService = filmService;
-
     }
 
-    @GetMapping("/films")
+    @GetMapping
     public Collection<FilmDto> getFilms() {
         return filmService.getAllFilms();
     }
 
-    @PostMapping("/films")
+    @PostMapping
     public FilmDto addFilm(@Valid @RequestBody Film film) {
         return filmService.createFilm(film);
     }
 
-    @PutMapping("/films")
+    @PutMapping
     public FilmDto updateFilm(@Valid @RequestBody Film newFilm) {
         return filmService.updateFilm(newFilm);
     }
 
 
-    @GetMapping("/films/{id}")
+    @GetMapping("/{id}")
     public FilmDto getFilmById(@PathVariable Integer id) {
         return filmService.getFilmById(id);
     }
 
-    @PutMapping("/films/{id}/like/{userId}")
+    @PutMapping("/{id}/like/{userId}")
     public void addLike(@PathVariable Integer id, @PathVariable Integer userId) {
         filmService.addLike(id, userId);
     }
 
-    @DeleteMapping("/films/{id}/like/{userId}")
+    @DeleteMapping("/{id}/like/{userId}")
     public void removeLike(@PathVariable Integer id, @PathVariable Integer userId) {
         filmService.deleteLike(userId, id);
     }
 
-    @GetMapping("/films/popular")
+    @GetMapping("/popular")
     public Collection<FilmDto> getPopular(@RequestParam(defaultValue = "10") int count) {
         return filmService.popularFilm(count);
     }
-
-    @GetMapping("/mpa")
-    public List<RatingMPADto> findAllMpa() {
-        return filmService.getAllMpa();
-    }
-
-    @GetMapping("/mpa/{id}")
-    public RatingMPADto findMpaById(@PathVariable int id) {
-        return filmService.getMpaById(id);
-    }
-
-    @GetMapping("/genres")
-    public List<GenreDto> findAllGenre() {
-        return filmService.getAllGenre();
-    }
-
-    @GetMapping("/genres/{id}")
-    public GenreDto findGenreById(@PathVariable int id) {
-        return filmService.getGenreById(id);
-    }
-
-
 }
